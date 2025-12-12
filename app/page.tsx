ca61ef5c62e423a48ae01d16bbfff7a1e7a3ea8c
Gufran Ahmed <gufranahmed0921@gmail.com>
2025-12-12 11:37:18 +0530

    Created Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import events from "@/lib/constants";
+import { title } from "process";
+import React from "react";
 
 const page = () => {
   return (
     <section>
-      <h1 className='text-center'>The Hub for Every Dev <br/> Event You Cant Miss</h1>
-      <p className='text-center mt-5'>Hackathons, Meetups and Conferences, All in One Place </p>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Cant Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups and Conferences, All in One Place{" "}
+      </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
